eabb7eb6e04656e62e3e043c1b62dff8200d6774
reiwruts215 <reiwruts@gmail.com>
2025-06-27 21:54:36 +0900

    profile image change

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "reiwruts",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/Kirby.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "이것저것",
     instagram: "reiwruts",
   },
